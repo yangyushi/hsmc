deec365613f86b3c9f59873a034bcda2e6cb7044
yangyushi <yangyushi1992@icloud.com>
2021-04-27 11:29:52 +0100

    update test

--- test/test.py
+++ test/test.py
@@ -1,12 +1,13 @@
-import chard_sphere
+from hsmc import chard_sphere
 
 
 n_particle = 500
 box = [10, 10, 10]  # box size in X, Y, and Z
 is_pbc = [True, True, False]  # no PBC in the z-direction
+is_hard = [False, False, True]  # hard walls in z-direction
 
 # create the system
-system = chard_sphere.HSMC(n_particle, box, is_pbc)
+system = chard_sphere.HSMC(n_particle, box, is_pbc, is_hard)
 
 # randomly fill non-overlapping hard spheres
 system.fill_hs()
